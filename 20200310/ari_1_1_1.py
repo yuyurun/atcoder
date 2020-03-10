2780491b719a060f0c3c00ffed380f691981859e
yuyurun <yuyu1.run@icloud.com>
2020-03-10 20:58:29 +0900

    2分探索,bisect,AC

--- 20200310/ari_1_1_1.py
+++ 20200310/ari_1_1_1.py
@@ -1,20 +1,20 @@
 n, b = map(int, input().split())
+a = [0] + [int(input()) for i in range(n)]
 
-a = [0]
-for i in range(n):
-    a.append(int(input()))
-ans = 0
+p = []
 for i in range(len(a)):
     for j in range(i+1):
-        for k in range(j+1):
-            for l in range(k+1):
-                if a[i] + a[j] + a[k] + a[l] < b and a[i] + a[j] + a[k] + a[l] > ans :
-                    ans = a[i] + a[j] + a[k] + a[l] 
+        pair = a[i] + a[j]
+        if pair <= b:
+            p.append(pair)
 
-
-    
-
-
+p.sort()
+ans = 0
+import bisect
+for i in p:
+    r = bisect.bisect_right(p, b-i)
+    tmp = i + p[r-1]
+    ans = max(tmp, ans)
 
 
 print(ans)
