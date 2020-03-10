7f415a6fa9f302513624794a957f8d7db20da335
yuyurun <yuyu1.run@icloud.com>
2020-03-10 20:19:57 +0900

    TLE

--- 20200310/ari_1_1_1.py
+++ 20200310/ari_1_1_1.py
@@ -3,14 +3,13 @@ n, b = map(int, input().split())
 a = [0]
 for i in range(n):
     a.append(int(input()))
-
 ans = 0
-for i1 in a:
-    for i2 in a:
-        for i3 in a:
-            for i4 in a:
-                if i1 + i2 + i3 + i4 < b and i1 + i2 + i3 + i4 > ans :
-                    ans = i1 + i2 + i3 + i4 
+for i in range(len(a)):
+    for j in range(i+1):
+        for k in range(j+1):
+            for l in range(k+1):
+                if a[i] + a[j] + a[k] + a[l] < b and a[i] + a[j] + a[k] + a[l] > ans :
+                    ans = a[i] + a[j] + a[k] + a[l] 
 
 
     
