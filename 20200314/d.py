a1380471090ba27bca4e3e1fee9ae30fda3641b7
yuyurun <yuyu1.run@icloud.com>
2020-03-15 01:56:59 +0900

    通るんかい

--- 20200314/d.py
+++ 20200314/d.py
@@ -3,22 +3,17 @@ n = int(input())
 
 ans = []
 d = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-fa = math.factorial(n)+1
-for i in range(math.factorial(n)):
-    tu = 'a'
-    for j in range(2, n+1):
-        if len(set(tu)) 
-        for k in range(j):
-            hh = i % j
-            if hh < k+1 and hh >= k:
-                tu += d[k]
-    ans.append(tu)
 
-for i in range(n):
-    for j, jj in enumerate(ans):
-        if not d[i] in jj:
-            for k in range(i+1, n):
-                if d[k] in jj:
-                    ans[j] = jj.replace(d[k], d[i])
-                    break
+todo = ['a']
+while len(todo)>0:
+    p = todo.pop(-1)
+    l = len(p)
+    cou = len(set(p))
+    if l < n:
+        for i in range(cou+1):
+            todo.append(p+d[i])
+    else:
+        ans.append(p)
+
+
 print('\n'.join(sorted(set(ans))))
